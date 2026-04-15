24612dfe1e598ca2268d6c9a4cfc169ee60876e1
SirealX <cesara.lunaperez@gmail.com>
2026-04-14 23:02:38 -0500

    CORS ERROR debugging

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://project-financeos.vercel.app/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
